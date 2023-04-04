d0fcab718933956c0cf06a97324328b26fceb623
Umair Jibran <me@umairjibran.com>
2023-04-04 16:42:52 +0500

    ♻️ ref: informs user about staged file before they enter a commit message
    * 🛠 config: add package to conform code style
    
    * 🐛 fix: fix spelling
    
    * 🐛 fix: informs user if no file staged
    
    * 🐛 fix: remove extra operations

--- src/commands.js
+++ src/commands.js
@@ -2,26 +2,30 @@ import { exec } from "child_process";
 import chalk from "chalk";
 import { logExec } from "./helper.js";
 
-export async function getStaggedFiles() {
-  return exec("git diff --cached --name-only", (err, stdout, stderr) => {
-    if (err) {
-      logExec(err);
-      process.exit(0);
-    }
-    if (stderr) {
-      logExec(stderr);
-      process.exit(0);
-    }
+export async function getStagedFiles() {
+  return new Promise((resolve, reject) => {
+    exec("git diff --cached --name-only", (err, stdout, stderr) => {
+      if (err) {
+        logExec(err);
+        reject(err);
+      }
+      if (stderr) {
+        logExec(stderr);
+        reject(stderr);
+      }
 
-    if (stdout.length <= 0) {
-      console.log(chalk.bgRed("⛔️ Whops! No files available to commit."));
-      console.log(
-        "💡 Make sure to use " +
-          chalk.bgWhite("git add") +
-          " before trying to commit."
-      );
-      process.exit(0);
-    }
+      if (stdout.length <= 0) {
+        console.log(chalk.bgRed("⛔️ Whops! No files available to commit."));
+        console.log(
+          "💡 Make sure to use " +
+            chalk.bgWhite("git add") +
+            " before trying to commit."
+        );
+        resolve(null);
+      } else {
+        resolve(stdout);
+      }
+    });
   });
 }
 
